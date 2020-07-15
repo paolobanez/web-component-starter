39084738c46eca1ab44e8c784ed938547145f5a0
paolo banez <paolo.banez@intechsive.com>
2020-07-16 02:51:57 +0800

    remove html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,6 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = ({ mode }) => {
   return {
     mode,
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-      }),
-    ],
     devtool: mode === 'development' ? 'source-map' : 'none',
   };
 };
